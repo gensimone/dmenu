8c9da98b613ab8f3d7f5d0581787bd1ecd92db21
Simone Gentili <gensimone.git@gmail.com>
2025-12-19 13:58:47 +0100

    Add vi-mode! :)

--- config.def.h
+++ config.def.h
@@ -12,6 +12,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#c5c8c6", "#1a1a1a" },
 	[SchemeSel] = { "#1a1a1a", "#c5c8c6" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+        [SchemeCursor] = { "#1a1a1a", "#c5c8c6"},
+
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
@@ -23,3 +25,16 @@ static unsigned int lines = 0;
 static const char worddelimiters[] = " ";
 
 static const int border_width = 3;
+
+/*
+ * -vi option; if nonzero, vi mode is always enabled and can be
+ * accessed with the global_esc keysym + mod mask
+ */
+static unsigned int vi_mode = 1;
+static unsigned int start_mode = 0;		/* 0 = insert mode, 1 = normal mode */
+static Key global_esc = { XK_n, Mod1Mask };	/* escape key when vi mode is not enabled explicitly */
+static Key quit_keys[] = {
+	/* keysym	modifier */
+	{ XK_Escape,		0 }
+};
+
