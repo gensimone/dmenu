cd6f1a8d659a40f99215d54e2ee7ecb64821a832
Simone Gentili <gensimone.git@gmail.com>
2025-12-26 00:25:57 +0100

    Update colorscheme and border size

--- config.def.h
+++ config.def.h
@@ -4,15 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack Nerd Font:size=12"
+    "JetBrainsMono Nerd Font:size=12"
 };
 static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c5c8c6", "#1a1a1a" },
-	[SchemeSel] = { "#1a1a1a", "#c5c8c6" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-        [SchemeCursor] = { "#1a1a1a", "#c5c8c6"},
+    /*     fg         bg       */
+    [SchemeNorm] = { "#c5c8c6", "#000000" },
+    [SchemeSel] = { "#000000", "#393963" },
+    [SchemeOut] = { "#000000", "#00ffff" },
+    [SchemeCursor] = { "#000000", "#c5c8c6"},
 
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -24,7 +24,7 @@ static unsigned int lines = 0;
  */
 static const char worddelimiters[] = " ";
 
-static const int border_width = 3;
+static const int border_width = 5;
 
 /*
  * -vi option; if nonzero, vi mode is always enabled and can be
@@ -34,7 +34,7 @@ static unsigned int vi_mode = 1;
 static unsigned int start_mode = 0;		/* 0 = insert mode, 1 = normal mode */
 static Key global_esc = { XK_n, Mod1Mask };	/* escape key when vi mode is not enabled explicitly */
 static Key quit_keys[] = {
-	/* keysym	modifier */
-	{ XK_Escape,		0 }
+    /* keysym	modifier */
+    { XK_Escape,		0 }
 };
 
