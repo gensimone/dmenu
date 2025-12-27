5dcadb113de145ce02b5a54f8c3948a15ea6c787
Simone Gentili <gensimone.git@gmail.com>
2025-12-27 22:02:09 +0100

    Update dmenu colorscheme and code format.

--- config.h
+++ config.h
@@ -1,18 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-    "JetBrainsMono Nerd Font:size=12"
-};
-static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"JetBrainsMono Nerd Font:size=12"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
-    [SchemeNorm] = { "#ebdbb2", "#282828" },
-    [SchemeSel] = { "#282828", "#ebdbb2" },
-    [SchemeOut] = { "#282828", "#00ffff" },
-    [SchemeCursor] = { "#282828", "#ebdbb2"},
+    [SchemeNorm] = {"#ffffff", "#1e1e1e"},
+    [SchemeSel] = {"#44bb44", "#000000"},
+    [SchemeOut] = {"#000000", "#00ffff"},
+    [SchemeCursor] = {"#000000", "#ffffff"},
 
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -31,9 +30,9 @@ static const int border_width = 5;
  * accessed with the global_esc keysym + mod mask
  */
 static unsigned int vi_mode = 1;
-static unsigned int start_mode = 0;		/* 0 = insert mode, 1 = normal mode */
-static Key global_esc = { XK_n, Mod1Mask };	/* escape key when vi mode is not enabled explicitly */
+static unsigned int start_mode = 0; /* 0 = insert mode, 1 = normal mode */
+static Key global_esc = {
+    XK_n, Mod1Mask}; /* escape key when vi mode is not enabled explicitly */
 static Key quit_keys[] = {
     /* keysym	modifier */
-    { XK_Escape,		0 }
-};
+    {XK_Escape, 0}};
